4b023dec93c3c0f41695fdd6b3f7352a0f46f5ac
”admin” <kaisei0201@gmail.com>
2020-07-30 07:45:04 +0000

    Profile作成画面用 1回目のコミットです

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,17 @@
-<!DOCTYPE html>
-<html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>MyProfile</title>
-</head>
+{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'Profileの新規作成')
 
-<body>
-    <h1>MyProfile作成画面</h1>
-</body>
-
-</html>
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <h2>MyProfile新規作成</h2>
+        </div>
+    </div>
+</div>
+@endsection
