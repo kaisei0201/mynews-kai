95ae2c5d1b59615de0e623acb64bc64ebff27d3d
”admin” <kaisei0201@gmail.com>
2020-08-04 07:12:28 +0000

    createから送信されてきたフォーム情報をデータベースに保存設定

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+// 以下を追記することでProfile Modelが扱えるようになる
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -11,12 +13,21 @@ class ProfileController extends Controller
     {
     return view('admin.profile.create');
     }
-
-    public function create()
+    public function create(Request $request)
     {
+    // Varidationを行う
+    $this->validate($request, Profile::$rules);
+    $profile = new Profile;
+    $form = $request->all();
+      // フォームから送信されてきた_tokenを削除する
+    unset($form['_token']);
+      // データベースに保存する
+    $profile->fill($form);
+    $profile->save();
+    return redirect('admin/profile/create');
+    // admin/profile/createにリダイレクトする
     return redirect('admin/profile/create');
     }
-
     public function edit()
     {
     return view('admin.profile.edit');
