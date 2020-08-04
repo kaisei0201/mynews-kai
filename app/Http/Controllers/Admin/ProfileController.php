0d1c85dd577711dca5399a87f14bd6c325525024
”admin” <kaisei0201@gmail.com>
2020-08-04 10:19:34 +0000

    editとupdate データベース保存するように実装

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -11,30 +11,45 @@ class ProfileController extends Controller
 {
     public function add()
     {
-    return view('admin.profile.create');
+        return view('admin.profile.create');
     }
     public function create(Request $request)
     {
-    // Varidationを行う
-    $this->validate($request, Profile::$rules);
-    $profile = new Profile;
-    $form = $request->all();
-      // フォームから送信されてきた_tokenを削除する
-    unset($form['_token']);
-      // データベースに保存する
-    $profile->fill($form);
-    $profile->save();
-    return redirect('admin/profile/create');
-    // admin/profile/createにリダイレクトする
-    return redirect('admin/profile/create');
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        // admin/profile/createにリダイレクトする
+        return redirect('admin/profile/create');
     }
-    public function edit()
+    public function edit(Request $request)
     {
-    return view('admin.profile.edit');
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+        return view('admin.profile.edit');
     }
 
-    public function update()
+    public function update(Request $request)
     {
-    return redirect('admin/profile/edit');
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        return redirect('admin/profile');
     }
 }
