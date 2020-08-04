3e6fc3082ae6d0acc1f5b70559a8013ecefc039f
”admin” <kaisei0201@gmail.com>
2020-08-04 10:22:27 +0000

    profile編集画面作成

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,16 +1,56 @@
-<!DOCTYPE html>
-<html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Profile</title>
-</head>
+{{-- profile.blade.phpの@yield('title')にProfileの編集'を埋め込む --}}
+@section('title', 'Profileの編集')
 
-<body>
-    <h1>Profile編集画面</h1>
-</body>
-
-</html>
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <h2>MyProfile編集画面</h2>
+            <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                @if (count($errors) > 0)
+                <!-- errors内に個数があるならば配列中の個数を返す -->
+                <ul>
+                    @foreach($errors->all() as $e)
+                    <!-- $errors内をループし、その中身を$eで表示する -->
+                    <li>{{ $e }}</li>
+                    @endforeach
+                </ul>
+                @endif
+                <div class="form-group row">
+                    <label class="col-md-2">氏名</label>
+                    <div class="col-md-10">
+                        <textarea class="form-control" name="name" rows="1">{{ old('body') }}</textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2">性別</label>
+                    <div class="col-md-10">
+                        <input type="radio" name="gender" value="male">男性
+                        <input type="radio" name="gender" value="male">女性
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2">趣味</label>
+                    <div class="col-md-10">
+                        <textarea class="form-control" name="hobby" rows="10">{{ old('body') }}</textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2">自己紹介欄</label>
+                    <div class="col-md-10">
+                        <textarea class="form-control" name="introduction" rows="12">{{ old('body') }}</textarea>
+                    </div>
+                </div>
+                <input type="hidden" name="id" value="{{ $profile_form->id }}">
+                {{ csrf_field() }}
+                <input type="submit" class="btn btn-primary" value="更新">
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
