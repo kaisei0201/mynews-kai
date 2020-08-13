e9c7f35cb74b64235b1cd900c14ad892e05bbcb4
”admin” <kaisei0201@gmail.com>
2020-08-13 03:59:57 +0000

    /profile にアクセスが来たら ProfileController/index Action に渡すように設定

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,21 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
     // news用Routing
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
     // profile用Routing
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile', 'Admin\ProfileController@index');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::get('profile/delete', 'Admin\ProfileController@delete');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index');
+Route::get('/profile', 'ProfileController@index');
