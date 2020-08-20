872f8785efa5fcaf3dea833ea179cf8620e7d67d
kaisei0201 <kaisei0201ok@gmail.com>
2020-08-20 22:03:55 +0900

    不具合対応

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -73,12 +73,13 @@ class NewsController extends Controller
         $this->validate($request, News::$rules);
         $news = News::find($request->id);
         $news_form = $request->all();
+        
         if ($request->remove == 'true') {
             $news_form['image_path'] = null;
         } elseif ($request->file('image')) {
             //$path = $request->file('image')->store('public/image');
             //$news_form['image_path'] = basename($path);
-            $path = Storage::disk('s3')->putFile('/',$news_form['image'],'public');
+            $path = Storage::disk('s3')->putFile('/', $news_form['image_path'],'public');
             $news->image_path = Storage::disk('s3')->url($path);
         } else {
             $news_form['image_path'] = $news->image_path;
